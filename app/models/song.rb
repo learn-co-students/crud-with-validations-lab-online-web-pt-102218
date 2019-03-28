9407b4c95aca577aa171b0896dcaf1ffdb282769
Roberto Samia <bojosteph@gmail.com>
2019-03-28 02:57:38 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ApplicationRecord
+    validates :title, presence: true, uniqueness: true
+    validates :released, inclusion: {in: [true, false]}
+    validates :artist_name, presence: true
+    validates :release_year, presence: true, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year}, if: ->{ released }  
 end
