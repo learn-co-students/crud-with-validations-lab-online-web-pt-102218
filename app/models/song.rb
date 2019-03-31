5c5a8c7560b7c98bc843e0cfcda2fcc0298058c5
github username <github email address>
2019-03-31 18:35:18 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,5 @@ class Song < ApplicationRecord
     validates :title, presence: true
     validates :title, uniqueness: true
     validates :release_year, presence: true
-    validates :release_year, absence: true
     validates :released, inclusion: {in: [true, false]}
 end
