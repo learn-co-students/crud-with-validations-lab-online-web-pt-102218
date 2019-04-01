4842aaa7bb12657fe1f9f4958acdac3b235311db
github username <github email address>
2019-04-01 18:30:30 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,5 +3,5 @@ class Song < ApplicationRecord
     validates :title, uniqueness: true
     validates :release_year, presence: true
     validates :released, inclusion: {in: [true, false]}
-    validates :release_year, length: {maximum: Time.now.year}
+    validates :release_year, numericality: {less_than: Time.now.year}
 end
