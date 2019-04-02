9bde4238a2ca9b3b1cfc73cc225682a4caa66311
github username <github email address>
2019-04-01 20:16:13 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,10 +3,5 @@ class Song < ApplicationRecord
     validates :title, presence: true
     validates :title, uniqueness: true
     validates :released, inclusion: {in: [true, false]}
-    validates :release_year, numericality: {less_than: Time.now.year}, if released
-
-    def released(song)
-        binding.pry
-        song.released == true
-    end
+    validates :release_year, numericality: {less_than: Time.now.year}, on: :released
 end
