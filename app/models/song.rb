4b1061802561bd76329c78b36dd07b2281962204
jaywood128 <jaywood128@gmail.com>
2019-04-07 15:15:42 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,19 @@
 class Song < ApplicationRecord
+    validates :title, presence: true
+    validates :released, inclusion: { in: [true, false] }
+    validate :valid_release_year?  
+    validates :artist_name, presence: true 
+    
+end
+
+def valid_release_year?
+   
+   if self.released == false 
+     true 
+    
+   elsif self.release_year != nil && self.release_year <= Date.current.year 
+     true 
+   else 
+    false 
+   end
 end
