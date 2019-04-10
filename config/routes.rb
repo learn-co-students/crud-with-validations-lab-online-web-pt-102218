9ff0daa70fb1b6b7d55b153d09476359a4644545
github username <github email address>
2019-04-10 09:11:26 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/songs/:id", to: "songs#show", as: "post"
+  get "/songs/:id/edit", to: "songs#edit", as: "edit_post"
+  patch "/songs/:id", to: "songs#update"
+  get "/songs/:id", to: "songs#show"
+  get "/songs/index", to: "songs#index"
+
 end
